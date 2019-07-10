2e22a975007078c8eaa6123b6bd1964d680376a6
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-10 23:12:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr, name){
+  let newArr = [];
+  for(let i = 0; i< arr.length ; i++)
+    newArr.push("Thank you, " + arr[i] +", for the wonderful " + name + " gift.");
+    
+    return newArr;
+}
+
+function countdown(num){
+  while(num >=0)
+  {
+    console.log(num);
+    num--;
+  }
+}
