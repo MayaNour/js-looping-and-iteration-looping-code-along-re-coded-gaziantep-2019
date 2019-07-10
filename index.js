957bce3fade6ed31c9448f0a10d479002df006f5
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-10 22:59:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
 function writeCards(arr, name){
   for(let i = 0; i< arr.length ; i++)
-    console.log("Thank you, " + arr[i] + ", for the wonderful "+ name + " gift.");
+    console.log("Thank you, ${arr[i]}, for the wonderful ${name} gift.");
 }
