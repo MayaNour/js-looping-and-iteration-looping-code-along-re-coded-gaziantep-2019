ed803f002cac0c9d438f9cdd9420fd667cfcd802
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-10 22:55:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Code your solutions in this file
-function 
+function writeCards(arr, name){
+  for(let i = 0; i< arr.length ; i++)
+    console.log("Thank you, " + arr[i] + ", for the wonderful "+ name + " gift.");
+}
